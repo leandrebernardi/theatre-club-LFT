c177052a310ced3320b5be286ba6c4cc25565383
leandrebernardi <leandrebernardimail@gmail.com>
2026-01-17 15:09:02 -0500

    Update script.js

--- script.js
+++ script.js
@@ -3,9 +3,9 @@ const decor = document.getElementById('decor');
 
 // On crée la liste de tes images dans l'ordre
 const listeImages = [
-    "https://media.istockphoto.com/id/1176123462/vector/modern-cafe-interior-empty-no-people-restaurant-cafeteria-design-flat-horizontal-vector.jpg?s=612x612&w=0&k=20&c=PwJmg9JELKEqtpN3k_GTJvD_6HuWckvN3GE2c4mlB-I=", // Image 1 (Café)
-    "https://i.ytimg.com/vi/HQMrFOIGBls/hq720.jpg?sqp=-oaymwEhCK4FEIIDSFryq4qpAxMIARUAAAAAGAElAADIQj0AgKJD&rs=AOn4CLAgVgWAUtUFEAKCJpNPxd-Hqqf5AA", // Image 2
-    "https://i.pinimg.com/736x/27/2d/56/272d56698720250225df2645eb5fafe2.jpg" // Image 3
+    "https://media.istockphoto.com/id/1176123462/vector/modern-cafe-interior-empty-no-people-restaurant-cafeteria-design-flat-horizontal-vector.jpg?s=612x612&w=0&k=20&c=PwJmg9JELKEqtpN3k_GTJvD_6HuWckvN3GE2c4mlB-I=", // Image 1 (Lunch Room)
+    "https://i.ytimg.com/vi/HQMrFOIGBls/hq720.jpg?sqp=-oaymwEhCK4FEIIDSFryq4qpAxMIARUAAAAAGAElAADIQj0AgKJD&rs=AOn4CLAgVgWAUtUFEAKCJpNPxd-Hqqf5AA", // Image 2(Rue)
+    "https://i.pinimg.com/736x/27/2d/56/272d56698720250225df2645eb5fafe2.jpg" // Image 3(Humain Camp)
 ];
 
 let indexActuel = 0; // On commence à la première image
